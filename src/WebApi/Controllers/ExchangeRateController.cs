70e0e6858920c71a65d2d5aea0de7d218ffa6fa0
AVitrum <andrey.almashi@gmail.com>
2024-12-31 00:02:28 +0200

    Fixed ExchangeRateController && ExchangeRateService

--- src/WebApi/Controllers/ExchangeRateController.cs
+++ src/WebApi/Controllers/ExchangeRateController.cs
@@ -1,5 +1,6 @@
 using Application.ExchangeRates.Results;
 using Domain.Common;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebApi.Controllers;
 
@@ -14,18 +15,22 @@ public class ExchangeRateController : ControllerBase
         _exchangeRateService = exchangeRateService;
     }
     
-    [HttpGet("fetch")]
+    [HttpPost("fetch")]
+    [Authorize(Roles = "Admin")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> FetchExchangeRatesAsync([FromQuery] ExchangeRatesRangeDto dto)
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> FetchExchangeRatesAsync(ExchangeRatesRangeDto dto)
     {
         BaseResult result = await _exchangeRateService.FetchExchangeRatesAsync(dto);
         return result.Success ? Ok() : BadRequest(result.Errors);
     }
     
     [HttpGet("getRange")]
+    [Authorize]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetRangeAsync([FromQuery] ExchangeRatesRangeDto dto)
     {
