3ef44fa98134b231f33c553e97efdb9fa845a4a4
AVitrum <andrey.almashi@gmail.com>
2025-04-15 20:53:36 +0300

    The login page has been renamed to the authorisation page. Added registration functionality. The colour scheme of the website has been changed

---
+++ src/UI/Pages/Auth.razor.cs
@@ -0,0 +1,103 @@
+using System.Net.Http.Json;
+using Domain.Enums;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Shared.Dtos;
+using Shared.Helpers;
+using Shared.Payload.Requests;
+using Shared.Payload.Responses;
+
+namespace UI.Pages;
+
+public partial class Auth : ComponentBase, IPageComponent
+{
+    private readonly LoginRequest _loginRequest = new LoginRequest
+    {
+        Provider = LoginManagerProvider.Default.ToString()
+    };
+
+    private readonly CreateUserDto _registrationRequest = new CreateUserDto
+    {
+        Provider = UserServiceProvider.DEFAULT.ToString()
+    };
+    
+    private bool IsLogin { get; set; } = true;
+
+    [Inject] private IJSRuntime Js { get; set; } = null!;
+    private string? _errorMessage;
+    
+    private async Task HandleLoginValidSubmit()
+    {
+        if (_loginRequest.Email is not null && !_loginRequest.Email.Contains("@"))
+        {
+            _loginRequest.UserName = _loginRequest.Email;
+            _loginRequest.Email = string.Empty;
+        }
+
+        try
+        {
+            HttpResponseMessage response = await Http.PostAsJsonAsync($"{UISettings.ApiUrl}/Identity/login", _loginRequest);
+            if (response.IsSuccessStatusCode)
+            {
+                LoginResponse? responseContent = await response.Content.ReadFromJsonAsync<LoginResponse>();
+                if (responseContent?.Token is not null)
+                {
+                    await JwtTokenHelper.SetJwtTokensInCookiesAsync(responseContent.Token, responseContent.RefreshToken, Js);
+                    ToastService.ShowSuccess("User successfully login!");
+                    Navigation.NavigateTo("/all-users", true);
+                }
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                string errorMessage = !string.IsNullOrEmpty(errorResponse)
+                    ? errorResponse
+                    : "Login failed. Please try again.";
+                
+                await HandleInvalidResponse(errorMessage);
+            }
+        }
+        catch (Exception)
+        {
+            await HandleInvalidResponse();
+        }
+    }
+    
+    private async Task HandleRegistrationValidSubmit()
+    {
+        HttpResponseMessage response = await Http.PostAsJsonAsync(
+            $"{UISettings.ApiUrl}/Identity/register", _registrationRequest);
+        
+        if (response.IsSuccessStatusCode)
+        {
+            ToastService.ShowSuccess("User successfully registered!");
+            Navigation.NavigateTo("/login", forceLoad: true);
+            
+        }
+        else
+        {
+            string errorResponse = await response.Content.ReadAsStringAsync();
+            string errorMessage = !string.IsNullOrEmpty(errorResponse)
+                ? errorResponse
+                : "Registration failed. Please try again.";
+            
+            await HandleInvalidResponse(errorMessage);
+        }
+    }
+    
+    public async Task HandleInvalidResponse(string message = "An error occurred while processing your request. Try again later.")
+    {
+        ToastService.ShowError(message);
+        
+        await Task.Delay(3000);
+        _errorMessage = null;
+        _loginRequest.Email = string.Empty;
+        _loginRequest.UserName = string.Empty;
+        _loginRequest.Password = string.Empty;
+    }
+
+    private void ToggleLoginMode()
+    {
+        IsLogin = !IsLogin;
+    }
+}
