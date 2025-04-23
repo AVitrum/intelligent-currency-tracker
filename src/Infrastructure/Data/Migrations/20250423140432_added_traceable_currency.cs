1994cf7e5dcdb72b769a58bf75547495fdb38c27
AVitrum <andrey.almashi@gmail.com>
2025-04-23 18:58:42 +0300

    feat:  - Added the ability to track currency.  - Added the ability to delete currency from the db. fix: Fixed a bug related to ExchangeRateSyncService.cs

---
+++ src/Infrastructure/Data/Migrations/20250423140432_added_traceable_currency.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_traceable_currency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TraceableCurrencies",
+                columns: table => new
+                {
+                    CurrencyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TimeStamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TraceableCurrencies", x => new { x.CurrencyId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_TraceableCurrencies_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TraceableCurrencies_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TraceableCurrencies_UserId",
+                table: "TraceableCurrencies",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TraceableCurrencies");
+        }
+    }
+}
