26311bc118fc5d9566e2cc5f937079932492460b
AVitrum <andrey.almashi@gmail.com>
2025-06-29 15:41:26 +0300

    feat: moved model into separate repository

--- src/Model/payload/requests/forecast_request.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class ForecastRequest(BaseModel):
-    currency_r030: int
-    periods: int
--- src/Model/payload/requests/predict_request.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class PredictRequest(BaseModel):
-    currency_r030: int
-    date: str
